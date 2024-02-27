d6441d8041d3f1226d03720d17a7c6aea9e6bd36
Arkadiusz Dawid <arkadiusz.dawid.99@gmail.com>
2024-02-27 13:02:28 +0100

    separate components and data

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    author: 'Savannah Guthrie',
+    title:
+      'Mostly What God Does: Reflections on Seeking and Finding His Love Everywhere',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Kristin Hannah',
+    title: 'The Women',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Jamie Kern Lima',
+    title: 'Worthy: How to Believe You Are Enough and Transform Your Life',
+    img: img3,
+    id: 3,
+  },
+]
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { books } from './books'
+import Book from './Book'
+
+const BookList = () => {
+  return (
+    <>
+      <h1>Amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} index={index} key={book.id} />
+        })}
+      </section>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
